f6362a17310fba9b6adb2fd7d7a1ea0c401305a1
1Pumpkinn <wishsmp1002@gmail.com>
2026-09-19 15:37:47 +1000

    ActionBar and TabList

--- src/main/java/net/rose/main/BarsAndTitles.java
+++ src/main/java/net/rose/main/BarsAndTitles.java
@@ -1,5 +1,7 @@
 package net.rose.main;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,6 +18,10 @@ public class BarsAndTitles implements Listener {
                 20, // 1 second
                 100, // 5 seconds
                 20); // 1 second
+
+        e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.DARK_RED + "BLOOD"));
+
+        e.getPlayer().setPlayerListHeaderFooter(ChatColor.RED + "Hello", "Meow\nSecond Line");
 
 
 
